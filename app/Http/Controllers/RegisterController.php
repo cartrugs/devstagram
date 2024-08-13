de9f0ef41804fd9c6ec500d4a4eb5d4c1ff1d1d6
cartrugs <cartrugs@gmail.com>
2024-08-13 15:23:50 +0200

    Vista de registro hecha. Rutas de get y post para registro hechas y funcionando

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index() 
+    {
+        return view('auth.register');
+    }
+
+    public function store()
+    {
+        dd('Post...');
+    }
+
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,28 +4,54 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
         <title>DevStagram - @yield('titulo')</title>
+        {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
         @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
 
     </head>
 
-    <body class="antialiased">
+    <body class="bg-gray-100">
 
-        <nav>
-            <a href="/">Página Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-            <a href="/contacto">Contacto</a>
-        </nav>
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                DevStagram
+                </h1>
+                
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
+                    {{-- <a href="/tienda">Tienda</a>
+                    <a href="/contacto">Contacto</a> --}}
+                </nav>
 
-        <!-- Directiva "yield" registra como un contenedor -->
+            </div>
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                <!-- Directiva "yield" registra como un contenedor -->
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        {{-- 
         <h1 class="text-4xl font-extrabold">@yield('titulo')<h1>
 
         <!-- Imprime una línea -->
         <hr>
 
-        @yield('contenido')
+        @yield('contenido') --}}
+
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados {{-- Fecha actual con Helper now()--}} {{ now()->year }}
+        </footer>
+
+        
+        
 
     </body>
 
