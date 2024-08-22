2e7e67d25eac93cc3689c439715d8cd2ab61f5b7
cartrugs <cartrugs@gmail.com>
2024-08-22 17:04:53 +0200

    Implementación de funcionalidades para eliminar comentarios relacionados al post que se elimina

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use auth;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -84,6 +85,13 @@ class PostController extends Controller
         
         $this->authorize('delete', $post);
         $post->delete();
+
+        // Eliminar la imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+        if (File::exists($imagen_path)) {
+            // Función de PHP
+            unlink($imagen_path);
+        }
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
--- database/migrations/2024_08_22_081144_create_comentarios_table.php
+++ database/migrations/2024_08_22_081144_create_comentarios_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('comentarios', function (Blueprint $table) {
             // Tabla de relación de muchos a muchos
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->string('comentario');
             $table->timestamps();
         });
